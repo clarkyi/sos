b1624f29eef9d07a761328284e1e55ebfd29bf8d
yiwenqi <yiwenqi@xiangrikui.com>
2015-03-09 13:56:15 +0800

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,12 +7,10 @@ set :repo_url, 'https://coding.net/clarkyi/sos.git'
 # Default branch is :master
 ask :branch, ask("deploy git branch: ","master")
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/sos'
-
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
+set :use_sudo, true
 # Default value for :format is :pretty
 # set :format, :pretty
 after 'deploy:publishing', 'deploy:restart'
@@ -21,16 +19,15 @@ after 'deploy:restart', 'deploy:cleanup'
 # set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
-
+set :rvm1_ruby_version, "2.0.0p598"
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
