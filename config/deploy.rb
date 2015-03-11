a06b04aa728755766cc04813af51c6a075b175c0
yiwenqi <yiwenqi@xiangrikui.com>
2015-03-11 12:00:04 +0800

    update capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, ask("deploy git branch: ","master")
 
 # Default value for :scm is :git
 set :scm, :git
-
+set :log_level, :trace 
 # set :use_sudo, true
 # Default value for :format is :pretty
 # set :format, :pretty
